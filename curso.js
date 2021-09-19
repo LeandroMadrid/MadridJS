b4df0d59c386b04043ef622746433d91d9481268
LeandroMadrid <90106007+LeandroMadrid@users.noreply.github.com>
2021-09-19 12:33:28 -0300

    Update curso.js

--- curso.js
+++ curso.js
@@ -1,4 +1,4 @@
 var añoActual = prompt("Ingrese el año actual");
 var añoUsuario = prompt("Ingrese su año de nacimiento");
 var edad = añoActual - añoUsuario;
-alert("Tu edad es " + edad + " años")
+alert("Tu edad es " + edad + " años");
