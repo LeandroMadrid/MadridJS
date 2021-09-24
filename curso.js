e5ee7be3a52fb75d50c245019da00c0ae182fc2e
Leandro Madrid <leandro.madrid@gmail.com>
2021-09-24 17:54:14 -0300

    Desefio JS con condicional

--- curso.js
+++ curso.js
@@ -1,4 +1,13 @@
-var añoActual = prompt("Ingrese el año actual");
-var añoUsuario = prompt("Ingrese su año de nacimiento");
-var edad = añoActual - añoUsuario;
-alert("Tu edad es " + edad + " años");
+var tamaño = prompt("Ingrese un tamaño entre 10 y 50");
+
+
+tamaño = parseInt(tamaño);
+
+
+if( tamaño>= 10 && tamaño<= 50){
+    alert("Tamaño correcto");
+}
+
+else{
+    alert("tamaño incorrecto");
+}
