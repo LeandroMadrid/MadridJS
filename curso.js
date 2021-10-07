aceb441c6928e2c3956842f7a801e35742e42c8e
Leandro Madrid <leandro.madrid@gmail.com>
2021-10-06 21:17:13 -0300

    commit con funciones relacionadas

--- curso.js
+++ curso.js
@@ -1,24 +1,19 @@
 function puedoComprar( edadUsuario ){
-
     if( edadUsuario >= 18){
-
         alert("Bienvenido a MILANO VINOS");
     }
     else if( edadUsuario < 18){
-
         alert("Prohibida la venta de alcohol a menores de 18 años");
     }
     else{
         alert("DATO INCORRECTO");
     }
-
 }
 
+function bienvenida(){
+    let edad = prompt("Por favor ingrese su edad");
+    edad = parseInt( edad );
+    puedoComprar( edad );
+}
 
-let edad = prompt("Por favor ingrese su edad");
-
-edad = parseInt( edad );
-
-puedoComprar( edad );
-
-
+bienvenida()
